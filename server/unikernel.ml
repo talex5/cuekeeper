3b24e5c984f9b758c2e14c1d73d97f942239ce05
Thomas Leonard <talex5@gmail.com>
2015-05-24 18:42:16 +0100

    Disable TLS for now
    See: https://github.com/mirage/mirage-tcpip/issues/140

--- server/unikernel.ml
+++ server/unikernel.ml
@@ -30,7 +30,7 @@ module Main (Stack:STACKV4) (Conf:KV_RO) (Clock:V1.CLOCK) = struct
   module TCP  = Stack.TCPV4
   module TLS  = Tls_mirage.Make (TCP)
   module X509 = Tls_mirage.X509 (Conf) (Clock)
-  module S = Cohttp_mirage.Server(TLS)
+  module S = Cohttp_mirage.Server(TCP)
 
   let respond_static segments =
     let path = String.concat "/" segments in
@@ -116,15 +116,18 @@ module Main (Stack:STACKV4) (Conf:KV_RO) (Clock:V1.CLOCK) = struct
     let http = S.make ~conn_closed:ignore ~callback:(handle_request s) () in
     X509.certificate conf `Default >>= fun cert ->
     let tls_config = Tls.Config.server ~certificates:(`Single cert) () in
-    Stack.listen_tcpv4 stack ~port:443 (fun flow ->
+    ignore tls_config;
+    Stack.listen_tcpv4 stack ~port:80 (fun flow ->
       let peer, port = TCP.get_dest flow in
       Log.info "Connection from %s (client port %d)" (Ipaddr.V4.to_string peer) port;
+(*
       TLS.server_of_flow tls_config flow >>= function
       | `Error _ -> Log.warn "TLS failed"; TCP.close flow
       | `Eof     -> Log.warn "TLS eof"; TCP.close flow
       | `Ok flow  ->
+*)
       S.listen http flow () () >>= fun () ->
-      TLS.close flow
+      TCP.close flow
     );
     Stack.listen stack
 
