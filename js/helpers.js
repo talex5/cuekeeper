44f4a1f10785220e9c63374dfd264c160d03b576
Thomas Leonard <talex5@gmail.com>
2015-10-09 15:42:48 +0100

    Get Cstruct JS helpers from Cstruct package

--- js/helpers.js
+++ js/helpers.js
@@ -1,28 +1,5 @@
 // TODO: These should have proper bounds checks.
 
-//Provides: caml_blit_string_to_bigstring
-//Requires: caml_string_unsafe_get
-function caml_blit_string_to_bigstring(buf1, ofs1, buf2, ofs2, len) {
-	for (var i=0; i<len; i++) {
-		buf2.set1(ofs2 + i, caml_string_unsafe_get(buf1, ofs1 + i));
-	}
-}
-
-//Provides: caml_blit_bigstring_to_string
-//Requires: caml_string_unsafe_set
-function caml_blit_bigstring_to_string(buf1, ofs1, buf2, ofs2, len) {
-	for (var i=0; i<len; i++) {
-		caml_string_unsafe_set(buf2, ofs2 + i, buf1.get1(ofs1 + i));
-	}
-}
-
-//Provides: caml_blit_bigstring_to_bigstring
-function caml_blit_bigstring_to_bigstring(buf1, ofs1, buf2, ofs2, len) {
-	for (var i=0; i<len; i++) {
-		buf2.set1(ofs2 + i, buf1.get1(ofs1 + i));
-	}
-}
-
 //Provides: bin_prot_blit_string_buf_stub
 //Requires: caml_blit_string_to_bigstring
 function bin_prot_blit_string_buf_stub(ofs1, buf1, ofs2, buf2, len) {
