c9209b8b872838f6b0228203c8c705ea5d29c740
Mindy Preston <meetup@yomimono.org>
2015-12-09 16:59:45 +0000

    add type constraints on branch_id and commit_id

--- utils/git_storage.mli
+++ utils/git_storage.mli
@@ -3,7 +3,8 @@
 
 (** A Git-like storage abstraction over Irmin. *)
 
-module Make (I : Irmin.S with type key = string list and type value = string) : sig
+module Make (I : Irmin.S with type key = string list and type value = string and
+            type commit_id = Irmin.Hash.SHA1.t and type branch_id = string) : sig
   include Git_storage_s.S
 
   val make : Irmin.config -> (string -> Irmin.task) -> Repository.t Lwt.t
